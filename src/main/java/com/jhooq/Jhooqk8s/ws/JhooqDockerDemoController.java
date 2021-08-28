b7a7169c3270bbbf57b3ce5b90346f8a498a0bfc
Nitin Garg <nitin194@gmail.com>
2021-08-28 09:44:44 +0530

    NA
    NA

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -8,6 +8,6 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello - Jhooq i Have updated the message";
+        return "Hello - Jhooq i doocker Have updated the message";
     }
 }
